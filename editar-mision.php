fc5f3e682dbf7d08e9a2c2ffd35183af211e0803
Julio Hurtado Jerves <juliens@outlook.com>
2019-03-28 17:58:03 -0500

    Fix eddit mission

--- editar-mision.php
+++ editar-mision.php
@@ -5,15 +5,15 @@
  * Date: 8/25/2017
  * Time: 11:29 AM
  */
+define('IN_MYBB', true);
+require_once '../global.php';
 
-require ('acceso.php');
-
-require('conexion.php');
-require('forum-conexion.php');
+require('acceso.php');
 require('reutilizar/fechas.php');
 
-if (!$acceso){
+if (!$acceso) {
     header('Location: error.php');
+    die;
 }
 
 ?>
@@ -25,7 +25,7 @@ if (!$acceso){
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Detalle de Misión - Ilvermorny.es</title>
+    <title>Detalle de Misión - Ilvermorny.xyz</title>
     <?php
     include('reutilizar/js.php');
     ?>
@@ -35,46 +35,57 @@ if (!$acceso){
 
 <body>
 
-<?php
-include ('reutilizar/menu.php');
-?>
+    <?php
+    include('reutilizar/menu.php');
+    ?>
 
-<div class="container">
-    <br><br>
-    <div class="panel panel-info">
-        <div class="panel-heading">Editar de la Misión</div>
-        <div class="panel-body">
-            <?php
+    <div class="container">
+        <br><br>
+        <div class="panel panel-info">
+            <div class="panel-heading">Editar de la Misión</div>
+            <div class="panel-body">
+                <?php
 
-            if(isset($_POST['save'])){
-                $id = mysqli_real_escape_string($db, $_POST['save']);
-                $name = mysqli_real_escape_string($db, $_POST['name']);
-                $init = mysqli_real_escape_string($db, $_POST['init']);
-                $end = mysqli_real_escape_string($db, $_POST['end']);
-                $creator = mysqli_real_escape_string($db, $_POST['user']);
-                $link = mysqli_real_escape_string($db, $_POST['link']);
-                $description = mysqli_real_escape_string($db, $_POST['description']);
-                $status = mysqli_real_escape_string($db, $_POST['status']);
-                $type = mysqli_real_escape_string($db, $_POST['type']);
-                $difficulty = mysqli_real_escape_string($db, $_POST['difficulty']);
+                if (isset($_POST['save'])) {
+                    $id = $db->escape_string($_POST['save']);
+                    $name = $db->escape_string($_POST['name']);
+                    $init = $db->escape_string($_POST['init']);
+                    $end = $db->escape_string($_POST['end']);
+                    $creator = $db->escape_string($_POST['user']);
+                    $link = $db->escape_string($_POST['link']);
+                    $description = $db->escape_string($_POST['description']);
+                    $status = $db->escape_string($_POST['status']);
+                    $type = $db->escape_string($_POST['type']);
+                    $difficulty = $db->escape_string($_POST['difficulty']);
 
-                $query = sprintf("UPDATE mision SET name = '%s', type = '%s', difficulty = '%s', init = '%s', end = '%s', link = '%s', description = '%s', status = '%s' WHERE id = '%s'",$name, $type, $difficulty, $init, $end, $link, $description, $status, $id);
-                $db->query($query);
-                header('Location: mision.php?id='.$id);
-            }
+                    $db->update_query("mission", array(
+                        'name' => $name,
+                        'type' => $type,
+                        'difficulty' => $difficulty,
+                        'init' => $init,
+                        'end' => $end,
+                        'link' => $link,
+                        'description' => $description,
+                        'status' => $status
+                    ), "id=$id");
+                    header('Location: mision.php?id=' . $id);
+                    die;
+                }
 
-            if (isset($_POST['mision'])) {
-                //$query = sprintf("SELECT * FROM mision T1 INNER JOIN usersinmission T2 ON T1.id = T2.id_mission WHERE T1.id = '%s'",$_REQUEST['id']);
-                $query = sprintf("SELECT * FROM mision WHERE id = '%s' LIMIT 1", $_POST['mision']);
-                $result = $db->query($query);
-                $mision = mysqli_fetch_array($result);
-            ?>
+                if (isset($_POST['mision'])) {
+                    //$query = sprintf("SELECT * FROM mision T1 INNER JOIN usersinmission T2 ON T1.id = T2.id_mission WHERE T1.id = '%s'",$_REQUEST['id']);
+                    $missionid = $db->escape_string($_POST['mision']);
+                    $result = $db->simple_select("mission", "*", "id=$missionid", array(
+                        "limit" => "1"
+                    ));
+                    $mision = $db->fetch_array($result);
+                    ?>
                 <form class="form-horizontal" role="form" method="post">
-                    <input type="hidden" name="save" value="<?php echo $_POST['mision']; ?>">
+                    <input type="hidden" name="save" value="<? echo $missionid; ?>">
                     <div class="form-group">
                         <label for="name" class="col-lg-2 control-label">Nombre</label>
                         <div class="col-lg-10">
-                            <input type="text" class="form-control" name="name" value="<?php echo $mision['name'];?>" required>
+                            <input type="text" class="form-control" name="name" value="<?php echo $mision['name']; ?>" required>
                         </div>
                     </div>
 
@@ -82,11 +93,11 @@ include ('reutilizar/menu.php');
                         <label for="type" class="col-lg-2 control-label">Tipo de Misión</label>
                         <div class="col-lg-10">
                             <select class="form-control" name="type">
-                                <option value="Normal" <?php if ($mision['type'] == 'Normal') echo "selected";?>>Normal</option>
-                                <option value="Bando" <?php if ($mision['type'] == 'Bando') echo "selected";?>>Bando</option>
-                                <option value="Cadena" <?php if ($mision['type'] == 'Cadena') echo "selected";?>>Cadena</option>
-                                <option value="Restringida" <?php if ($mision['type'] == 'Restringida') echo "selected";?>>Restringida</option>
-                                <option value="Limite de Tiempo" <?php if ($mision['type'] == 'Limite de Tiempo') echo "selected";?>>Limite de Tiempo</option>
+                                <option value="Normal" <? if ($mision['type'] == 'Normal') echo "selected"; ?>>Normal</option>
+                                <option value="Bando" <? if ($mision['type'] == 'Bando') echo "selected"; ?>>Bando</option>
+                                <option value="Cadena" <? if ($mision['type'] == 'Cadena') echo "selected"; ?>>Cadena</option>
+                                <option value="Restringida" <? if ($mision['type'] == 'Restringida') echo "selected"; ?>>Restringida</option>
+                                <option value="Limite de Tiempo" <? if ($mision['type'] == 'Limite de Tiempo') echo "selected"; ?>>Limite de Tiempo</option>
                             </select>
                         </div>
                     </div>
@@ -95,10 +106,10 @@ include ('reutilizar/menu.php');
                         <label for="difficulty" class="col-lg-2 control-label">Dificultad</label>
                         <div class="col-lg-10">
                             <select class="form-control" name="difficulty">
-                                <option value="A" <?php if ($mision['difficulty'] == 'A') echo "selected";?>>A</option>
-                                <option value="AA" <?php if ($mision['difficulty'] == 'AA') echo "selected";?>>AA</option>
-                                <option value="AAA" <?php if ($mision['difficulty'] == 'AAA') echo "selected";?>>AAA</option>
-                                <option value="Especial" <?php if ($mision['difficulty'] == 'Especial') echo "selected";?>>Especial</option>
+                                <option value="A" <? if ($mision['difficulty'] == 'A') echo "selected"; ?>>A</option>
+                                <option value="AA" <? if ($mision['difficulty'] == 'AA') echo "selected"; ?>>AA</option>
+                                <option value="AAA" <? if ($mision['difficulty'] == 'AAA') echo "selected"; ?>>AAA</option>
+                                <option value="Especial" <? if ($mision['difficulty'] == 'Especial') echo "selected"; ?>>Especial</option>
                             </select>
                         </div>
                     </div>
@@ -107,14 +118,14 @@ include ('reutilizar/menu.php');
                     <div class="form-group">
                         <label for="init" class="col-lg-2 control-label">Fecha de Inicio</label>
                         <div class="col-lg-10">
-                            <input type="date" class="form-control" name="init" value="<?php echo $mision['init'];?>" required>
+                            <input type="date" class="form-control" name="init" value="<? echo $mision['init']; ?>" required>
                         </div>
                     </div>
 
                     <div class="form-group">
                         <label for="end" class="col-lg-2 control-label">Fecha de Finalización</label>
                         <div class="col-lg-10">
-                            <input type="date" class="form-control" name="end" value="<?php echo $mision['end'];?>">
+                            <input type="date" class="form-control" name="end" value="<? echo $mision['end']; ?>">
                         </div>
                     </div>
 
@@ -122,8 +133,8 @@ include ('reutilizar/menu.php');
                         <label for="status" class="col-lg-2 control-label">Estado de la Misión</label>
                         <div class="col-lg-10">
                             <select class="form-control" name="status">
-                                <option value="1" <?php if ($mision['status'] == 1) echo "selected";?>>Abierta</option>
-                                <option value="2" <?php if ($mision['status'] == 2) echo "selected";?>>Cerrada</option>
+                                <option value="1" <? if ($mision['status'] == 1) echo "selected"; ?>>Abierta</option>
+                                <option value="2" <? if ($mision['status'] == 2) echo "selected"; ?>>Cerrada</option>
                             </select>
                         </div>
                     </div>
@@ -134,11 +145,12 @@ include ('reutilizar/menu.php');
                         <label for="user" class="col-lg-2 control-label">Master de la Misión</label>
                         <div class="col-lg-10">
                             <?php
-                            $query_user = sprintf("SELECT * from mybb_users WHERE uid = '%s' LIMIT 1", $mision['user']);
-                            $resultuser = $db2->query($query_user);
-                            $user = mysqli_fetch_array($resultuser);
+                            $resultuser = $db->simple_select("users", "*", "uid=" . $mision['user'], array(
+                                "limit" => 1
+                            ));
+                            $user = $db->fetch_array($resultuser);
                             ?>
-                            <input type="text" class="form-control" name="user" value="<?php echo $user['username'];?>" disabled>
+                            <input type="text" class="form-control" name="user" value="<?php echo $user['username']; ?>" disabled>
                         </div>
                     </div>
 
@@ -147,9 +159,9 @@ include ('reutilizar/menu.php');
                         <div class="col-lg-10">
                             <div class="input-group">
                                 <div class="input-group-btn">
-                                    <a href="<?php echo $mision['link'];?>" type="button" class="btn btn-default" aria-haspopup="true" aria-expanded="false" target="_blank">Ver</a>
+                                    <a href="<?php echo $mision['link']; ?>" type="button" class="btn btn-default" aria-haspopup="true" aria-expanded="false" target="_blank">Ver</a>
                                 </div>
-                                <input type="url" class="form-control" name="link" value="<?php echo $mision['link'];?>" required>
+                                <input type="url" class="form-control" name="link" value="<?php echo $mision['link']; ?>" required>
                             </div>
                         </div>
                     </div>
@@ -157,39 +169,39 @@ include ('reutilizar/menu.php');
                     <div class="form-group">
                         <label for="description" class="col-lg-2 control-label">Descripción</label>
                         <div class="col-lg-10">
-                            <textarea class="form-control" rows="10" name="description" required><?php echo $mision['description'];?></textarea>
+                            <textarea class="form-control" rows="10" name="description" required><?php echo $mision['description']; ?></textarea>
                         </div>
                     </div>
 
                     <?php
 
-                        if ($mybb->usergroup['cancp'])
-                        {
-                            //echo "<form action='editar-mision.php' method='post'>";
-                            echo sprintf("<input type='hidden' value='%s' name='mision'>", $_POST['mision']);
-                            echo "<button type='submit' type=\"button\" class=\"btn btn-default btn-primary btn-block\">";
-                            echo "Guardar";
-                            echo "</button>";
-                            echo "<br>";
-                        }
-
-                        echo sprintf("<a href='mision.php?id=%s' type=\"button\" class=\"btn btn-default btn-danger btn-block\">", $_POST['mision']);
-                        echo "Cancelar";
-                        echo "</a>";
+                    if ($mybb->usergroup['cancp']) {
+                        //echo "<form action='editar-mision.php' method='post'>";
+                        echo sprintf("<input type='hidden' value='%s' name='mision'>", $_POST['mision']);
+                        echo "<button type='submit' type=\"button\" class=\"btn btn-default btn-primary btn-block\">";
+                        echo "Guardar";
+                        echo "</button>";
                         echo "<br>";
                     }
 
+                    echo sprintf("<a href='mision.php?id=%s' type=\"button\" class=\"btn btn-default btn-danger btn-block\">", $_POST['mision']);
+                    echo "Cancelar";
+                    echo "</a>";
+                    echo "<br>";
+                }
 
 
-                    ?>
+
+                ?>
 
                 </form>
 
 
 
+            </div>
         </div>
-    </div>
 
-</div>
+    </div>
 </body>
-</html>
+
+</html> 
